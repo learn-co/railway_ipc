1e5be4cdc0c9d3336865c918fa220a9e0085eb77
Natalie Perpepaj <natalie@flatironschool.com>
2019-07-24 16:54:50 -0400

    Set default adapters for Publisher

--- lib/learn_ipc_ex/publisher.ex
+++ lib/learn_ipc_ex/publisher.ex
@@ -1,17 +1,17 @@
 defmodule LearnIpcEx.Publisher do
   defmacro __using__(opts) do
     quote do
-      @stream_adapter Application.get_env(:learn_ipc_ex, :stream_adapter)
-      @payload_converter Application.get_env(:learn_ipc_ex, :payload_converter)
+      @stream_adapter Application.get_env(:learn_ipc_ex, :stream_adapter, LearnIpcEx.RabbitMQ.RabbitMQAdapter)
+      @payload_converter Application.get_env(:learn_ipc_ex, :payload_converter, LearnIpcEx.RabbitMQ.Payload)
 
       def publish(message) do
+        {:ok, message} = @payload_converter.encode(message)
         @stream_adapter.publish(
           LearnIpcEx.Connection.channel(),
           unquote(Keyword.get(opts, :exchange)),
-          @payload_converter.encode(message)
+          message
         )
       end
     end
   end
 end
-
