13b2907e65cf1a140459abe4f728f1e3bfde2658
Steven Nunez <steven.nunez@gmail.com>
2019-07-27 17:17:46 -0400

    Name Connection process

--- lib/railway_ipc/supervisor.ex
+++ lib/railway_ipc/supervisor.ex
@@ -1,12 +1,14 @@
 defmodule RailwayIpc.Connection.Supervisor do
   use Supervisor
 
-  def start_link(additional_children) do
+  def start_link(additional_children \\ []) do
     Supervisor.start_link(__MODULE__, additional_children, name: __MODULE__)
   end
 
   def init(additional_children) do
-    children = [RailwayIpc.Connection] ++ additional_children
+    children = [
+                 {RailwayIpc.Connection, name: RailwayIpc.Connection}
+               ] ++ additional_children
     Supervisor.init(children, strategy: :rest_for_one)
   end
 end
