acbace0563f7dc51a8cf8e6e6f46d6de0bbc8b3e
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-12-10 15:43:42 -0500

    fix supervisor setup

--- lib/railway_ipc/application.ex
+++ lib/railway_ipc/application.ex
@@ -3,6 +3,7 @@ defmodule RailwayIpc.Application do
 
   use Application
   @dev_repo Application.get_env(:railway_ipc, :dev_repo)
+
   def start(_type, _args) do
     opts = [strategy: :one_for_one, name: RailwayIpc.Supervisor]
     Supervisor.start_link(children(@dev_repo, Mix.env()), opts)
@@ -11,22 +12,15 @@ defmodule RailwayIpc.Application do
   def children(true, :dev) do
     [
       RailwayIpc.Dev.Repo,
-      {
-        RailwayIpc.Connection.Supervisor,
-        [RailwayIpc.Ipc.RepublishedMessagesConsumer]
-      }
+      {RailwayIpc.Connection.Supervisor, []}
     ]
   end
 
   def children(true, :test) do
-    [RailwayIpc.Dev.Repo]
+    [
+      RailwayIpc.Dev.Repo
+    ]
   end
 
-  def children(nil, _),
-    do: [
-      {
-        RailwayIpc.Connection.Supervisor,
-        [RailwayIpc.Ipc.RepublishedMessagesConsumer]
-      }
-    ]
+  def children(_), do: []
 end
