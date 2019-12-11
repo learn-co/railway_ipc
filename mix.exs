8ed0594fc244c3808e34acf7d825e819cd654b8c
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-12-11 13:39:45 -0500

    bump version no

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RailwayIpc.MixProject do
   def project do
     [
       app: :railway_ipc,
-      version: "0.2",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
