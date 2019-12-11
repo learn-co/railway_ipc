edf9db2cdf4c1134aeae2af3ff8b9a12a1b3676b
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-12-11 13:36:34 -0500

    fix version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RailwayIpc.MixProject do
   def project do
     [
       app: :railway_ipc,
-      version: "0.1.9",
+      version: "0.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
