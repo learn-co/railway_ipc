749849218bbb562ee3ecee4e4a66cd453e4eb885
Steven Nuñez <steven@flatironschool.com>
2020-02-07 17:11:04 -0500

    Add UUID on command and response

--- lib/railway_ipc/command_message_handler.ex
+++ lib/railway_ipc/command_message_handler.ex
@@ -19,12 +19,18 @@ defmodule RailwayIpc.CommandMessageHandler do
   defp prepare_event(event, message) do
     event
     |> update_context(message)
+    |> update_user_uuid(message)
     |> update_correlation_id(message)
   end
 
   defp update_context(new_event, previous_event) do
     new_event
     |> update_in([Access.key!(:context)], &merge_context(&1, previous_event.context))
+  end
+
+  defp update_user_uuid(new_event, previous_event) do
+    new_event
+    |> Map.put(:user_uuid, previous_event.user_uuid)
   end
 
   defp merge_context(nil, prev_context), do: prev_context
