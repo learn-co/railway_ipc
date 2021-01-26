6c620a86ed49051ec5402911059e166141bd35fe
Bob Nadler <bnadlerjr@gmail.com>
2021-01-26 15:11:41 -0500

    Add MessageFormat.JsonProtobuf module
    Used for (de|en)coding protobufs as JSON. Nothing is using this module
    yet. There's a fair amount of duplication between this module and the
    `MessageFormat.BinaryProtobuf` module, but I'm fine w/ it for the time
    being since there's still some refactoring to do.

---
+++ lib/railway_ipc/core/message_format/json_protobuf.ex
@@ -0,0 +1,139 @@
+defmodule RailwayIpc.Core.MessageFormat.JsonProtobuf do
+  @moduledoc """
+  _This is an internal module, not part of the public API._
+
+  Messages that use the `JsonProtobuf` format have the following
+  characteristics:
+
+  * The payload s a struct that contains two attributes: `type` and
+    `encoded_message`
+
+  * The `type` attribute is the name of the Elixir module for the protobuf
+    without the "Elixir" prefix and uses colon notation instead of dots
+
+  * The `encoded_message` attribute is the protobuf in map form
+
+  * The entire payload is then converted to JSON
+
+  Note:
+  I _think_ the reason for converting to colon notation is an artifact of
+  wanting to be compatable with the Ruby version since Ruby classes use
+  colon notation. -BN
+
+  """
+
+  alias RailwayIpc.DefaultMessage
+
+  @doc """
+  Encodes the given `protobuf` by creating a JSON string with two attributes:
+
+  * `type` -- the Protobuf module name as a string using colon notation
+  * `encoded_message` -- the Protobuf converted to a map
+
+  """
+  def encode(protobuf) when is_map(protobuf) or is_atom(protobuf) do
+    protobuf
+    |> build_payload()
+    |> encode_type()
+    |> encode_message()
+    |> encode_payload_as_json()
+  end
+
+  def encode(_), do: {:error, "Argument Error: Valid Protobuf required"}
+
+  @doc """
+  Decodes the given `message` into a Protobuf `struct`.
+
+  """
+  def decode(message) when not is_binary(message) do
+    {:error, "Malformed JSON given. Must be a string. (#{inspect(message)})"}
+  end
+
+  def decode(message) do
+    message
+    |> decode_json()
+    |> parse_type()
+    |> check_that_module_is_defined()
+    |> hydrate_protobuf_from_map()
+  end
+
+  defp build_payload(protobuf), do: {:ok, %{}, protobuf}
+
+  defp encode_type({:ok, payload, protobuf}) do
+    type =
+      protobuf.__struct__
+      |> to_string
+      |> replace_invalid_chars()
+
+    {:ok, Map.put(payload, :type, type), protobuf}
+  rescue
+    KeyError ->
+      {:error, "Argument Error: Valid Protobuf required"}
+  end
+
+  defp encode_message({:ok, payload, protobuf}) do
+    encoded_message = protobuf |> Map.from_struct()
+    {:ok, Map.put(payload, :encoded_message, encoded_message), protobuf}
+  end
+
+  defp encode_message({:error, _} = error), do: error
+
+  defp encode_payload_as_json({:ok, payload, _}) do
+    case Jason.encode(payload) do
+      {:ok, json} -> {:ok, json, payload.type}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp encode_payload_as_json({:error, _} = error), do: error
+
+  defp type_to_module(type) do
+    type
+    |> String.split("::")
+    |> Module.concat()
+  end
+
+  defp replace_invalid_chars(module_name_string) do
+    Regex.replace(~r/\AElixir\./, module_name_string, "")
+    |> String.replace(".", "::")
+  end
+
+  defp hydrate_protobuf_from_map({:error, _} = error), do: error
+
+  defp hydrate_protobuf_from_map({status, payload}) do
+    %{module: module, encoded_message: message, type: type} = payload
+    decoded_message = message |> module.new()
+    {status, decoded_message, type}
+  rescue
+    Protocol.UndefinedError ->
+      {:error, "Cannot decode protobuf"}
+  end
+
+  defp check_that_module_is_defined({:ok, payload}) do
+    %{module: module} = payload
+    module.__info__(:module)
+    {:ok, payload}
+  rescue
+    UndefinedFunctionError ->
+      {:unknown_message_type, Map.put(payload, :module, DefaultMessage)}
+  end
+
+  defp check_that_module_is_defined({:error, _} = error), do: error
+
+  defp parse_type({:ok, %{type: type} = payload}) when is_binary(type) do
+    {:ok, Map.put(payload, :module, type_to_module(type))}
+  end
+
+  defp parse_type({:ok, %{type: type}}) when not is_binary(type) do
+    {:error, "Message `type` attribute must be a string"}
+  end
+
+  defp parse_type({:ok, _}), do: {:error, "Message is missing the `type` attribute"}
+  defp parse_type({:error, _} = error), do: error
+
+  defp decode_json(json) do
+    {:ok, Jason.decode!(json, keys: :atoms)}
+  rescue
+    Jason.DecodeError -> {:error, "Message is invalid JSON (#{json})"}
+  end
+end
---
+++ test/railway_ipc/core/message_format/json_protobuf_test.exs
@@ -0,0 +1,89 @@
+defmodule RailwayIpc.Core.MessageFormat.JsonProtobufTest do
+  use ExUnit.Case, async: true
+
+  alias RailwayIpc.Core.MessageFormat.JsonProtobuf
+  alias RailwayIpc.DefaultMessage
+
+  describe "#encode" do
+    test "encode a protobuf" do
+      msg = Events.AThingWasDone.new(uuid: "abc123")
+
+      expected = {
+        :ok,
+        ~S({"encoded_message":{"context":{},"correlation_id":"",) <>
+          ~S("data":null,"user_uuid":"","uuid":"abc123"},) <>
+          ~S("type":"Events::AThingWasDone"}),
+        "Events::AThingWasDone"
+      }
+
+      assert expected == JsonProtobuf.encode(msg)
+    end
+
+    test "only valid protobufs can be encoded" do
+      {:error, error} = JsonProtobuf.encode("foo")
+      assert "Argument Error: Valid Protobuf required" == error
+    end
+
+    test "bare structs cannot be encoded" do
+      {:error, error} = JsonProtobuf.encode(%{foo: 1})
+      assert "Argument Error: Valid Protobuf required" == error
+    end
+  end
+
+  describe "#decode" do
+    test "decode a message to a protobuf" do
+      msg = Events.AThingWasDone.new(uuid: "abc123")
+      {:ok, encoded, _type} = JsonProtobuf.encode(msg)
+
+      expected = {
+        :ok,
+        %Events.AThingWasDone{
+          context: %{},
+          correlation_id: "",
+          user_uuid: "",
+          uuid: "abc123"
+        },
+        "Events::AThingWasDone"
+      }
+
+      assert expected == JsonProtobuf.decode(encoded)
+    end
+
+    test "message must be a string" do
+      {:error, error} = JsonProtobuf.decode(:foo)
+      assert "Malformed JSON given. Must be a string. (:foo)" == error
+    end
+
+    test "message must be valid JSON" do
+      {:error, error} = JsonProtobuf.decode("not_json")
+      assert "Message is invalid JSON (not_json)" == error
+    end
+
+    test "message must include a type attribute" do
+      {:error, error} = JsonProtobuf.decode("{}")
+      assert "Message is missing the `type` attribute" == error
+    end
+
+    test "message protobuf type must be string" do
+      message = %{type: 1} |> Jason.encode!()
+      {:error, error} = JsonProtobuf.decode(message)
+      assert "Message `type` attribute must be a string" == error
+    end
+
+    test "use default message protobuf if module is not defined" do
+      message = %{type: "NotAModule", encoded_message: %{}} |> Jason.encode!()
+      {:unknown_message_type, proto, type} = JsonProtobuf.decode(message)
+      assert DefaultMessage == proto.__struct__
+      assert "NotAModule" == type
+    end
+
+    test "enclosed encoded message must be a decodable protobuf" do
+      message =
+        %{type: "Events.AThingWasDone", encoded_message: "invalid"}
+        |> Jason.encode!()
+
+      {:error, error} = JsonProtobuf.decode(message)
+      assert "Cannot decode protobuf" == error
+    end
+  end
+end
